cc437850b9750e609abb792ec630dec63658965d
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-08-15 23:45:46 +0200

    refactor(umd.wrapper): remove it from source code
    umd wrapper is added on the build phase

---
+++ tasks/umd.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dist: {
+    src: 'lib/<%= pkg.name.replace(/\.js$/, "") %>.js',
+    dest: 'dist/<%= pkg.name.replace(/\.js$/, "") %>.js', // optional, if missing the src will be used
+    indent: '  ',
+    objectToExport: 'mintool'
+  }
+}
