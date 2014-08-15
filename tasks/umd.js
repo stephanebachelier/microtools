e11c356424bde084e15e6724365ef66bf9ebb27a
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-08-16 00:26:01 +0200

    fix(name): remove last reference to old name

--- tasks/umd.js
+++ tasks/umd.js
@@ -3,6 +3,6 @@ module.exports = {
     src: 'lib/<%= pkg.name.replace(/\.js$/, "") %>.js',
     dest: 'dist/<%= pkg.name.replace(/\.js$/, "") %>.js', // optional, if missing the src will be used
     indent: '  ',
-    objectToExport: 'mintool'
+    objectToExport: 'microtools'
   }
 }
